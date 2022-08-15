b70160fb085b3ba020dd9a7e104ee6a3581f4155
Andrea Fang <arhughes43@gmail.com>
2022-08-15 15:23:46 -0700

    all typeDefs

--- server/schemas/typeDefs.js
+++ server/schemas/typeDefs.js
@@ -1,50 +1,49 @@
 // define the necessary Query and Mutation types
 
-// Query type:
+const { gql } = require('apollo-server-express');
+// TODO: investigate id returned from google book API (they are Strings)
+const typeDefs = gql`
+    type User {
+        _id: ID
+        username: String
+        email: String
+        bookCount: Int
+        savedBooks: [Book]
+    }
 
-//     me: Which returns a User type.
+    type Book {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
 
-// Mutation type:
+    input BookInput {
+        bookId: String
+        authors: [String]
+        description: String
+        title: String
+        image: String
+        link: String
+    }
 
-//     login: Accepts an email and password as parameters; returns an Auth type.
+    type Auth {
+        token: String
+        user: User
+    }
 
-//     addUser: Accepts a username, email, and password as parameters; returns an Auth type.
+    type Query {
+        me: User
+    }
 
-//     saveBook: Accepts a book author's array, description, title, bookId, image, and link as parameters; returns a User type. 
-//     (Look into creating what's known as an input type to handle all of these parameters!)
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        saveBook(input: BookInput): User 
+        removeBook(bookId: ID!): User
+    }
+`;
 
-// removeBook: Accepts a book's bookId as a parameter; returns a User type.
-
-// User type:
-
-//     _id
-
-//     username
-
-//     email
-
-//     bookCount
-
-//     savedBooks (This will be an array of the Book type.)
-
-// Book type:
-
-//     bookId (Not the _id, but the book's id value returned from Google's Book API.)
-
-//     authors (An array of strings, as there may be more than one author.)
-
-//     description
-
-//     title
-
-//     image
-
-//     link
-
-// Auth type:
-
-//     token
-
-//     user (References the User type.)
-
-
+module.exports = typeDefs;
